b69e1de041eb00072c30139d0aea8052e1fb3613
Sgohil590 <gohilsahil513@gmail.com>
2025-06-23 15:14:29 +0530

    ecr details add

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
-
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 947557066309.dkr.ecr.ap-south-1.amazonaws.com
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker pull python01:latest 947557066309.dkr.ecr.ap-south-1.amazonaws.com/python01:latest
+docker run -d -p 5000:5000  python01:latest 947557066309.dkr.ecr.ap-south-1.amazonaws.com/python01:latest
