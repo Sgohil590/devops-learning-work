48e641ab80df0daebf039491b9b195b23685a91a
Sgohil590 <gohilsahil513@gmail.com>
2025-06-23 15:44:31 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 947557066309.dkr.ecr.ap-south-1.amazonaws.com
-# Run the Docker image as a container
-docker pull python01:latest 947557066309.dkr.ecr.ap-south-1.amazonaws.com/python01:latest
-docker run -d -p 5000:5000  python01:latest 947557066309.dkr.ecr.ap-south-1.amazonaws.com/python01:latest
+
